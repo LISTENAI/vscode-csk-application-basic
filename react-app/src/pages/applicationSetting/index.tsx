cea300084193f4df459078302a98d430dd9d2996
xzma <xzma@listenai.com>
2022-12-05 16:17:45 +0800

    [feature]优化版型配置逻辑

--- react-app/src/pages/applicationSetting/index.tsx
+++ react-app/src/pages/applicationSetting/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import ReactDom from 'react-dom'
 import './index.less'
-import { Button, Form, Row, Col, Select, ConfigProvider, Spin } from 'antd'
+import { Button, Form, Row, Col, Select, ConfigProvider, Spin, message } from 'antd'
 const { Option } = Select
 import { postMessage } from '../../../utils'
 const CreateApplication: React.FC = () => {
   const [form] = Form.useForm()
   const [boards, setBoards] = useState<string[]>([])
+  const [originBoard, setOriginBoard] = useState<string>()
   const [loading, setLoading] = useState(true)
   const [btnLoading, setBtnLoading] = useState(false)
 
@@ -20,11 +21,14 @@ const CreateApplication: React.FC = () => {
       case 'getBoards':
         const { board, list } = message.data
         setBoards(list)
+        setOriginBoard(board)
         form.setFieldsValue({ board })
         setLoading(false)
         break
       case 'saveApplicationSetting':
         setBtnLoading(false)
+        setOriginBoard(message.data)
+        form.setFieldsValue({ board: message.data })
         break
       default:
         break
@@ -32,10 +36,20 @@ const CreateApplication: React.FC = () => {
   }
 
   const onFinish = (values: any) => {
+    const { board } = values
+    if (board !== originBoard) {
+      showConfirm(values)
+    } else {
+      message.info(`当前应用版型已是${board}`)
+    }
+  }
+  const saveHandle = (values: any) => {
     setBtnLoading(true)
     postMessage('saveApplicationSetting', values)
   }
-
+  const showConfirm = (values: any) => {
+    saveHandle(values)
+  }
   const formItemLayout = {
     labelCol: {
       xs: { span: 24 },
--- src/main/application/setting.ts
+++ src/main/application/setting.ts
@@ -6,37 +6,65 @@
  */
 import { CreatePanel, Page } from '../../utils/panel';
 import * as vscode from 'vscode';
-import { Application } from './create'
+import { Application } from './create';
+import { pathExists, remove } from 'fs-extra';
+import * as path from 'path';
+
 export class CreateSettingPanel extends CreatePanel {
+    board: string | undefined;
     constructor(extensionPath: string, pageData: Page) {
         super(extensionPath, pageData);
         const self = this;
         this.panel.webview.onDidReceiveMessage(async message => {
             switch (message.type) {
                 case 'getBoards':
-                    console.log('getBoards--->')
+                    console.log('getBoards--->');
                     const res: string = await Application.getBoardList();
                     const board: string = await Application.getBoard();
+                    this.board = board;
                     self.panel.webview.postMessage({
                         type: "getBoards",
                         data: {
                             list: res.split(/\r?\n/),
-                            board: board || ''
+                            board: board || '',
                         }
                     });
 
                     break;
                 case 'saveApplicationSetting':
                     try {
-                        await Application.setBoard(message.data.board);
-                        vscode.window.showInformationMessage('设置成功')
+                        const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
+                            ? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
+                        const hasBuildDir: boolean = !!(rootPath && await pathExists(path.resolve(rootPath, 'build')));
+                        if (hasBuildDir) {
+                            const options: vscode.MessageOptions = { detail: '该应用已存在编译目录，切换版型后将清除编译产物目录，', modal: true };
+                            vscode.window.showInformationMessage('是否确认切换应用版型', options, ...["确定"]).then(async (val) => {
+                                if (val === '确定') {
+                                    rootPath && await remove(path.resolve(rootPath, 'build'));
+                                    this.board = message.data.board;
+
+                                    await Application.setBoard(message.data.board);
+                                    vscode.window.showInformationMessage('设置成功');
+                                }
+                                self.panel.webview.postMessage({
+                                    type: "saveApplicationSetting",
+                                    data: self.board
+                                });
+                            });
+                        } else {
+                            await Application.setBoard(message.data.board);
+                            this.board = message.data.board;
+                            vscode.window.showInformationMessage('设置成功');
+                            self.panel.webview.postMessage({
+                                type: "saveApplicationSetting",
+                                data: self.board
+                            });
+                        }
+
                     } catch {
-                        vscode.window.showErrorMessage('设置失败,请重试')
+                        vscode.window.showErrorMessage('设置失败,请重试');
                     }
-                    self.panel.webview.postMessage({
-                        type: "saveApplicationSetting",
-                        data:''
-                    });
+
                     break;
                 default:
                     return;
